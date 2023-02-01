26d65e3f8b07daa3ce7541d7fc404992afed9b48
WHY-Hugo <hugo.tw.wang@gmail.com>
2023-02-01 23:29:52 +0800

    Site updated: 2023-02-01 23:29:52

--- js/main.js
+++ js/main.js
@@ -1,74 +1,65 @@
-/* global KEEP */
+/* global REDEFINE */
 
 window.addEventListener('DOMContentLoaded', () => {
-  const { version, local_search, code_block, code_copy, lazyload } = KEEP.theme_config
 
-  KEEP.themeInfo = {
-    theme: `Keep v${version}`,
-    author: 'XPoet',
-    repository: 'https://github.com/XPoet/hexo-theme-keep'
+  REDEFINE.themeInfo = {
+    theme: `Redefine v${REDEFINE.theme_config.version}`,
+    author: 'EvanNotFound',
+    repository: 'https://github.com/EvanNotFound/hexo-theme-redefine'
   }
 
-  KEEP.localStorageKey = 'KEEP-THEME-STATUS'
+  REDEFINE.localStorageKey = 'REDEFINE-THEME-STATUS';
 
-  KEEP.styleStatus = {
+  REDEFINE.styleStatus = {
     isExpandPageWidth: false,
     isDark: false,
     fontSizeLevel: 0,
-    isShowToc: true
+    isOpenPageAside: true
   }
 
   // print theme base info
-  KEEP.printThemeInfo = () => {
-    console.log(
-      `\n %c ${KEEP.themeInfo.theme} %c ${KEEP.themeInfo.repository} \n`,
-      `color: #fadfa3; background: #333; padding: 6px 0;`,
-      `padding: 6px 0;`
-    )
+  REDEFINE.printThemeInfo = () => {
+    console.log(`      ______ __  __  ______  __    __  ______                       \r\n     \/\\__  _\/\\ \\_\\ \\\/\\  ___\\\/\\ \"-.\/  \\\/\\  ___\\                      \r\n     \\\/_\/\\ \\\\ \\  __ \\ \\  __\\\\ \\ \\-.\/\\ \\ \\  __\\                      \r\n        \\ \\_\\\\ \\_\\ \\_\\ \\_____\\ \\_\\ \\ \\_\\ \\_____\\                    \r\n         \\\/_\/ \\\/_\/\\\/_\/\\\/_____\/\\\/_\/  \\\/_\/\\\/_____\/                    \r\n                                                               \r\n ______  ______  _____   ______  ______ __  __   __  ______    \r\n\/\\  == \\\/\\  ___\\\/\\  __-.\/\\  ___\\\/\\  ___\/\\ \\\/\\ \"-.\\ \\\/\\  ___\\   \r\n\\ \\  __<\\ \\  __\\\\ \\ \\\/\\ \\ \\  __\\\\ \\  __\\ \\ \\ \\ \\-.  \\ \\  __\\   \r\n \\ \\_\\ \\_\\ \\_____\\ \\____-\\ \\_____\\ \\_\\  \\ \\_\\ \\_\\\\\"\\_\\ \\_____\\ \r\n  \\\/_\/ \/_\/\\\/_____\/\\\/____\/ \\\/_____\/\\\/_\/   \\\/_\/\\\/_\/ \\\/_\/\\\/_____\/\r\n                                                               \r\n  Github: https:\/\/github.com\/EvanNotFound\/hexo-theme-redefine`);
   }
 
   // set styleStatus to localStorage
-  KEEP.setStyleStatus = () => {
-    localStorage.setItem(KEEP.localStorageKey, JSON.stringify(KEEP.styleStatus))
+  REDEFINE.setStyleStatus = () => {
+    localStorage.setItem(REDEFINE.localStorageKey, JSON.stringify(REDEFINE.styleStatus));
   }
 
   // get styleStatus from localStorage
-  KEEP.getStyleStatus = () => {
-    let temp = localStorage.getItem(KEEP.localStorageKey)
+  REDEFINE.getStyleStatus = () => {
+    let temp = localStorage.getItem(REDEFINE.localStorageKey);
     if (temp) {
-      temp = JSON.parse(temp)
-      for (let key in KEEP.styleStatus) {
-        KEEP.styleStatus[key] = temp[key]
+      temp = JSON.parse(temp);
+      for (let key in REDEFINE.styleStatus) {
+        REDEFINE.styleStatus[key] = temp[key];
       }
-      return temp
+      return temp;
     } else {
-      return null
+      return null;
     }
   }
 
-  KEEP.refresh = () => {
-    KEEP.initUtils()
-    KEEP.initHeaderShrink()
-    KEEP.initModeToggle()
-    KEEP.initBack2Top()
+  REDEFINE.refresh = () => {
+    REDEFINE.initUtils();
+    REDEFINE.initMenuShrink();
+    REDEFINE.initModeToggle();
+    REDEFINE.initBackToTop();
 
-    if (local_search?.enable === true) {
-      KEEP.initLocalSearch()
+    if (REDEFINE.theme_config.local_search.enable === true) {
+      REDEFINE.initLocalSearch();
     }
 
-    if (
-      code_block?.tools?.enable === true ||
-      code_block?.enable === true ||
-      code_copy?.enable === true
-    ) {
-      KEEP.initCodeBlockTools()
+    if (REDEFINE.theme_config.code_block.copy === true) {
+      REDEFINE.initCopyCode();
     }
 
-    if (lazyload?.enable === true) {
-      KEEP.initLazyLoad()
+    if (REDEFINE.theme_config.lazyload.enable === true) {
+      REDEFINE.initLazyLoad();
     }
   }
 
-  KEEP.printThemeInfo()
-  KEEP.refresh()
-})
+  REDEFINE.printThemeInfo();
+  REDEFINE.refresh();
+});
